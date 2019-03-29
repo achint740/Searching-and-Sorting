6a6e3d4b29494a7830e410b7268fb0568e62d308
Achint <42889869+achint740@users.noreply.github.com>
2019-03-29 11:07:36 +0530

    Create count-sort2.cpp
    Problem Link : https://www.hackerrank.com/challenges/countingsort2/problem

---
+++ count-sort2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int pos[100];
+    for(int i=0;i<n;i++)
+      cin>>arr[i];
+    for(int i=0;i<100;i++)
+     {
+         pos[i]=0;
+     }
+    for(int i=0;i<n;i++)
+     {
+         pos[arr[i]]++;
+     }
+    for(int i=0;i<100;i++)
+     {
+        for(int j=0;j<pos[i];j++)
+          {
+              cout<<i<<" ";
+          }
+     }
+}
