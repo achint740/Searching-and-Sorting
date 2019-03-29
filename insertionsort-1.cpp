f1dbfdbb6686a78d3445a7e26c64c5c9cbb9fb2a
Achint <42889869+achint740@users.noreply.github.com>
2019-03-29 11:04:32 +0530

    Create insertionsort-1.cpp
    Problem Link : https://www.hackerrank.com/challenges/insertionsort1/problem

---
+++ insertionsort-1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int n,i,j,num;
+     cin>>n;
+     int a[n];
+     for(i=0;i<n;i++)
+       cin>>a[i];
+    num=a[n-1];
+    for(i=n-1;i>0;i--)
+     {
+         if(a[i-1]>=num){
+           a[i]=a[i-1];
+        for(j=0;j<n;j++)
+           cout<<a[j]<<" ";
+           cout<<endl;
+         }
+          else
+          {
+              a[i]=num;
+               for(j=0;j<n;j++)
+           cout<<a[j]<<" ";
+           cout<<endl;
+           break;
+          }
+     }
+     if(i==0)
+     {
+         a[0]=num;
+       for(j=0;j<n;j++)
+           cout<<a[j]<<" ";
+           cout<<endl;
+     }
+ }
+
