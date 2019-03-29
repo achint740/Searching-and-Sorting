f9b9e10281982509d0020fa95c86d2e8b992526b
Achint <42889869+achint740@users.noreply.github.com>
2019-03-29 10:58:43 +0530

    Create bigsort.cpp
    Problem Link : https://www.hackerrank.com/challenges/big-sorting/problem

---
+++ bigsort.cpp
@@ -0,0 +1,56 @@
+//Big Sorting
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool myCondition(string x,string y)
+ {
+     bool ans;
+     if(x.length()!=y.length())
+       ans = x.length()<y.length();
+     else
+       ans = x<y;
+    return ans;
+ }
+
+// Complete the bigSorting function below.
+vector<string> bigSorting(vector<string> unsorted) {
+  sort(unsorted.begin(),unsorted.end(),myCondition);
+  return unsorted;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> unsorted(n);
+
+    for (int i = 0; i < n; i++) {
+        string unsorted_item;
+        getline(cin, unsorted_item);
+
+        unsorted[i] = unsorted_item;
+    }
+
+    vector<string> result = bigSorting(unsorted);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
