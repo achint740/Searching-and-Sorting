c340cf24f7aec89641ddaf54ecaa0b535600789b
Achint <42889869+achint740@users.noreply.github.com>
2019-08-12 14:15:19 +0530

    Create FindMedian.cpp
    Problem Link : https://www.hackerrank.com/challenges/find-the-median/problem

---
+++ FindMedian.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n,i,j,key;
+ cin>>n;
+ int a[n];
+ for(i=0;i<n;i++)
+   cin>>a[i];
+ for(i=1;i<n;i++)
+  {
+      j = i-1;
+      key=a[i];
+      while(j>=0 && a[j]>key)
+       {
+           a[j+1] = a[j];
+           j--;
+       }
+       a[j+1] = key;
+  }
+  cout<<a[(n)/2];
+}
